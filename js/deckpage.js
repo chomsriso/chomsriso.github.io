b904c2292ae934dbea933f7590949cdde46f9946
chomsriso <chom18sriso@gmail.com>
2025-05-30 14:48:47 +0700

    Update deckpage.js

--- js/deckpage.js
+++ js/deckpage.js
@@ -8,8 +8,8 @@ function setColorScheme(scheme) {
 		prev[0].src = "https://chomsriso.github.io/src/icon_nextPic-dark.svg";
       		break;
     	case 'light':
-        	next.src = "https://chomsriso.github.io/src/icon_nextPic.svg";
-        	prev.src = "https://chomsriso.github.io/src/icon_nextPic.svg";
+        	next[0].src = "https://chomsriso.github.io/src/icon_nextPic.svg";
+        	prev[0].src = "https://chomsriso.github.io/src/icon_nextPic.svg";
       		break;
   	}
 }
@@ -35,61 +35,10 @@ if(window.matchMedia){
 }
 
 updateColorScheme();
-
-
-const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-if (isMobile) {
-	const element = document.getElementById('swipeArea');
-	element.addEventListener('touchstart', handleTouchStart, false);        
-	element.addEventListener('touchmove', handleTouchMove, false);
-
-	var xDown = null;                                                        
-	var yDown = null;
-}
-
-function getTouches(evt) {
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
-}                                                     
-                                                                         
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-}                                                
-                                                                         
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
-
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
-
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-                                                                         
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-            /* right swipe */ 
-            plusSlides(-1);
-        } else {
-            /* left swipe */
-            plusSlides(1);
-        }                       
-    } else {
-        if ( yDiff > 0 ) {
-            /* down swipe */ 
-        } else { 
-            /* up swipe */
-        }                                                                 
-    }
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
-}
     
 let slideIndex = 1;
+let isPaged = false;
+let dots = document.getElementsByClassName("dot");
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -100,18 +49,28 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+function getWhere(ele) {
+	if (!isPag) {
+		let rawIndex = (ele.scrollLeft / ( ele.scrollWidth / dots.length ))  + 1 ;
+    	slideIndex = Math.round(rawIndex);
+        for (let i = 0; i < dots.length; i++) {
+          	dots[i].className = dots[i].className.replace(" active", "");
+        }
+		dots[slideIndex-1].className += " active";
+  	}    
+    
+    isPag = false;
+}
+
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  	let showarea = document.getElementById("swipeArea");
+  	let x = showarea.scrollWidth / dots.length ;
+  	if (n > dots.length) {slideIndex = 1}    
+  	if (n < 1) {slideIndex = dots.length}  
+  	for (let i = 0; i < dots.length; i++) {
+    	dots[i].className = dots[i].className.replace(" active", "");
+  	}
+  	dots[slideIndex-1].className += " active";
+  	isPag = true;
+  	showarea.scrollLeft = (slideIndex-1) * x.toFixed();
 }

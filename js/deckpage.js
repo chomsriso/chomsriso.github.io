7430adaac906a2f130651fa5e2964d9198cf05cd
chomsriso <chom18sriso@gmail.com>
2025-06-15 17:34:40 +0700

    Update deckpage.js

--- js/deckpage.js
+++ js/deckpage.js
@@ -1,41 +1,4 @@
 
-function setColorScheme(scheme) {
-	var next = document.getElementsByClassName('next');
-	var prev = document.getElementsByClassName('prev');
-	switch(scheme){
-    	case 'dark':
-		next[0].src = "https://chomsriso.github.io/src/icon_nextPic-dark.svg";
-		prev[0].src = "https://chomsriso.github.io/src/icon_nextPic-dark.svg";
-      		break;
-    	case 'light':
-        	next[0].src = "https://chomsriso.github.io/src/icon_nextPic.svg";
-        	prev[0].src = "https://chomsriso.github.io/src/icon_nextPic.svg";
-      		break;
-  	}
-}
-
-function getPreferredColorScheme() {
-  if (window.matchMedia) {
-    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
-      return 'dark';
-    } else {
-      return 'light';
-    }
-  }
-  return 'light';
-}
-
-function updateColorScheme(){
-    setColorScheme(getPreferredColorScheme());
-}
-
-if(window.matchMedia){
-  var colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
-  colorSchemeQuery.addEventListener('change', updateColorScheme);
-}
-
-updateColorScheme();
-    
 let slideIndex = 1;
 let isPaged = false;
 let dots = document.getElementsByClassName("dot");

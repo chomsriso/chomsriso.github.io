004ad61ba78a1cdc9888a65243e0f3e792f6c244
chomsriso <chom18sriso@gmail.com>
2025-06-22 22:03:28 +0700

    Update randomCard.js

--- js/randomCard.js
+++ js/randomCard.js
@@ -2,7 +2,7 @@ function randomCard(){
 	let card  = document.getElementById("thecard");
     	let randnum = Math.floor(Math.random() * 42);
     
-    	card.src = "https://chomsriso.github.io/src/NW-deck-h500/PNG-" + randnum.toString() + ".png";
+    	card.src = "https://chomsriso.github.io/src/NW-deck/PNG-" + randnum.toString() + ".png";
     	card.addEventListener('load', () => {
 	  	// Perform actions after the image has loaded
 	  	card.style.animation = 'none';
